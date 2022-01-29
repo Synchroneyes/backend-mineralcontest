323a0f5a6cae08a6af2a7188e427367191b13d9e
Synchroneyes <ximylzx@gmail.com>
2022-01-29 11:57:18 +0100

    feat: init

---
+++ src/main/java/fr/mc/monvoisinkevin/backendMineralcontest/MineralContestBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.mc.monvoisinkevin.backendMineralcontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineralContestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineralContestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/mc/monvoisinkevin/backendMineralcontest/MineralContestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mc.monvoisinkevin.backendMineralcontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineralContestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
