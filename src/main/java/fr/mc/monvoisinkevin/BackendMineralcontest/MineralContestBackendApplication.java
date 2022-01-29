a3e1b140616c03610446babbff6b555a093d7701
Synchroneyes <ximylzx@gmail.com>
2022-01-29 16:48:48 +0100

    feat: add deployment

--- src/main/java/fr/mc/monvoisinkevin/BackendMineralcontest/MineralContestBackendApplication.java
+++ src/main/java/fr/mc/monvoisinkevin/BackendMineralcontest/MineralContestBackendApplication.java
@@ -3,7 +3,11 @@ package fr.mc.monvoisinkevin.BackendMineralcontest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MineralContestBackendApplication extends SpringBootServletInitializer {
 
@@ -11,4 +15,9 @@ public class MineralContestBackendApplication extends SpringBootServletInitializ
 		SpringApplication.run(MineralContestBackendApplication.class, args);
 	}
 
+	@GetMapping
+	@ResponseBody
+	public String currentUserName() {
+		return "Hello world";
+	}
 }
